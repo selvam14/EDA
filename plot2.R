66e0fa7bcf2bff2ae925a396859eb91320fdf649
selvam14 <selvam.mohan14@gmail.com>
2015-09-13 14:36:33 +0800

    Project 1

---
+++ plot2.R
@@ -0,0 +1,11 @@
+setwd('C://Users//User//Documents//eda')
+dt=read.csv('household_power_consumption.txt',header=T, sep=';')
+dt$Datetime = paste(as.character(dt[,1]) , dt[,2])
+dt[,1]=as.Date(dt$Date,'%d/%m/%Y')
+dt = subset(dt, Date == '2007-02-01' | Date == '2007-02-02')
+dt[,3] = as.numeric(as.character(dt[,3]))
+dt$dt <- strptime(dt$Datetime, '%d/%m/%Y %H:%M')
+#Generate PNG graph
+png('plot2.png',width=480,height=480)
+plot(dt$dt,dt$Global_active_power,ylab='Global Active Power (kilowatts)',xlab='',type='l')
+dev.off()
